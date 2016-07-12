53719ba6bd2256aa1bd016ce353cc1b175461b85
Andy Zheng <tz7@nyu.edu>
2016-07-12 16:59:19 +0800

    init

---
+++ src/js/compress/lib.js
@@ -0,0 +1,9 @@
+var Library = {
+    name: "Timmy",
+    greet: function(){
+        alert("Hello from the " + Library.name + " library.");
+    }
+}
+$(document).ready(function(){
+
+});
---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+//var _ = require('underscore'),
+//  names = ['Bruce Wayne', 'Wally West', 'John Jones', 'Kyle Rayner', 'Arthur Curry', 'Clark Kent'],
+//  otherNames = ['Barry Allen', 'Hal Jordan', 'Kara Kent', 'Diana Prince', 'Ray Palmer', 'Oliver Queen'];
+//
+//_.each([names, otherNames], function(nameGroup) {
+//  findSuperman(nameGroup);
+//});
+//
+//function findSuperman(values) {
+//  _.find(values, function(name) {
+//    if (name === 'Clark Kent') {
+//      console.log('It\'s Superman!');
+//    } else {
+//      console.log('... No superman!');
+//    }
+//  });
+//}
+window.$ = window.jQuery = require('jquery');
+var bootstrapjs = require('bootstrap-sass');
